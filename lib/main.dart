e8d26eccc384960c66576be5c96ce4a9b657b805
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-02-18 20:44:11 +0530

    Second Project

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import './ui/home.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: "Layouts",
+    // it fills the entire view
+    home: new Home(),
+  ));
+}
